7be50d96636983f5a44b7070c616e936d811198b
ALeksandar12d <brlegal2000@abv.bg>
2022-03-09 16:17:47 +0200

    Commit 1

---
+++ Sport and Style LTD/Models/CategoryVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_and_Style_LTD.Models
+{
+    public class CategoryVM
+    {
+    }
+}
